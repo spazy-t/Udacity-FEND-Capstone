5577aaa206b17648774017631949bbba39e47f00
Tom Schrimshaw <tschrimshaw@gmail.com>
2020-05-04 13:17:12 +0100

    refactor: adds to server test

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -4,8 +4,7 @@ const request = require('supertest')
 describe('app get(/geo/:place)', () => {
     test('gets geo data and send it back', async () => {
         const res = await request(app).get('/geo/'+'Doncaster')
-        //const response = 
         expect(res.status).toBe(200)
-        //expect(res)
+        expect(res.text).toContain('Doncaster')
     })
 })
